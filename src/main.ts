e803fd05f8f3f8b7e29d031c9de5661735f7c0b1
BambusControl <mojmir.majer@gmail.com>
2023-01-14 23:55:55 +0100

    Remove unused settings model

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,17 @@
 import {App, Plugin, PluginManifest} from "obsidian";
-import {UnicodeSearchPluginSettings} from "./data/model/unicode-search-plugin.settings";
 import {FuzzySearchModal} from "./components/fuzzy-search.modal";
 import {UnicodeCharacterStorage} from "./service/unicode-character.storage";
 import {ConstantUnicodeCharacterStorage} from "./service/constant-unicode-character.storage";
 
 export default class UnicodeSearchPlugin extends Plugin {
 
-	public settings: UnicodeSearchPluginSettings;
-
 	private readonly service: UnicodeCharacterStorage;
 
 	public constructor(
 		app: App,
 		manifest: PluginManifest,
-		settings: UnicodeSearchPluginSettings,
 	) {
 		super(app, manifest);
-		this.settings = settings;
 		this.service = new ConstantUnicodeCharacterStorage();
 	}
 
