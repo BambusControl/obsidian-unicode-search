006f233328c3295b75b9126b5b8fe9df151a238b
BambusControl <mojmir.majer@gmail.com>
2023-05-15 21:54:26 +0200

    Remove main plugin implementation

--- src/unicode-search/main.ts
+++ src/unicode-search/main.ts
@@ -1,18 +1,6 @@
 import {App, Plugin, PluginManifest} from "obsidian";
-import {UsageTrackedStorage} from "./service/storage/usage-tracked.storage";
-import {FuzzySearchModal} from "./components/fuzzy-search.modal";
-import {PluginDataService} from "./service/plugin-data.service";
-import {StatTrackedStorage} from "./service/storage/stat-tracked.storage";
-import {DataService} from "./service/data.service";
-import {DataAccess} from "./service/data.access";
-import {UcdService} from "./service/ucd.service";
 
 export default class UnicodeSearchPlugin extends Plugin {
-
-	private services?: {
-		dataService: DataService & DataAccess,
-		usageTrackedStorage: StatTrackedStorage,
-	};
 
 	public constructor(
 		app: App,
@@ -21,45 +9,5 @@ export default class UnicodeSearchPlugin extends Plugin {
 		super(app, manifest);
 	}
 
-	public override async onload(): Promise<void> {
-		const dataService = new PluginDataService(this);
-		const usageTrackedStorage = new UsageTrackedStorage(dataService);
-
-		this.services = {
-			dataService: dataService,
-			usageTrackedStorage: usageTrackedStorage,
-		};
-
-		await UnicodeSearchPlugin.initializeData(dataService, new UcdService());
-
-		super.addCommand({
-			id: "search-unicode-chars",
-			name: "Search Unicode characters",
-
-			editorCallback: editor => {
-				const modal = new FuzzySearchModal(
-					app,
-					editor,
-					dataService,
-					usageTrackedStorage,
-				);
-				modal.open();
-				return true;
-			},
-		});
-	}
-
-	private static async initializeData(dataService: DataService, ucdService: UcdService): Promise<void> {
-		const initialized = await dataService.isInitialized();
-
-		if (initialized) {
-			return;
-		}
-
-		const data = await ucdService.fetchCharacters();
-
-		await dataService.exportData(data);
-		await dataService.setAsInitialized();
-	}
-
 }
+
