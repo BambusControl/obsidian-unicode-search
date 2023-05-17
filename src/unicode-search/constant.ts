607c6721873fd6dbcf346366bbb27e8c94eb417d
BambusControl <mojmir.majer@gmail.com>
2023-05-17 19:15:21 +0200

    More initialization constants

--- src/unicode-search/constant.ts
+++ src/unicode-search/constant.ts
@@ -1,13 +1,37 @@
-import {SaveDataMeta} from "./types";
+import {SaveData, SaveDataMeta, SaveDataPrimary, SaveDataSettings} from "./types";
+import {Shape} from "./shape";
 
 /*
 * The easiest way to make sure this value doesn't get modified is to create a new object each time.
 */
 
-export function INITIAL_SAVE_DATA(): SaveDataMeta {
+export function INITIAL_SAVE_DATA(): Shape<SaveData> {
+	return {
+		meta: null,
+		settings: null,
+		primary: null,
+	};
+}
+
+export function INITIAL_SAVE_DATA_META(): SaveDataMeta {
 	return {
 		meta: {
 			version: "0.4.0",
 		},
+	}
+}
+
+export function INITIAL_SAVE_DATA_SETTINGS(): SaveDataSettings {
+	return {
+		settings: {
+		},
+	}
+}
+
+export function INITIAL_SAVE_DATA_PRIMARY(): SaveDataPrimary {
+	return {
+		primary: {
+			characters: []
+		}
 	}
 }
