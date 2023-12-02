877521c4cae2cffc1b08792771ff2c02d2fcbf3f
Bambus Control <mojmir.majer@gmail.com>
2023-12-02 20:48:52 +0100

    Settings from template

--- src/unicode-search/main.ts
+++ src/unicode-search/main.ts
@@ -4,6 +4,7 @@ import {FuzzySearchModal} from "./components/fuzzy-search.modal";
 import {PluginDataService} from "./service/plugin-data.service";
 import {DataService} from "./service/data.service";
 import {UcdService} from "./service/ucd.service";
+import {SettingTab} from "./components/settings.tab"
 
 /* Used by Obsidian */
 // noinspection JSUnusedGlobalSymbols
@@ -17,7 +18,9 @@ export default class UnicodeSearchPlugin extends Plugin {
 	}
 
 	public override async onload(): Promise<void> {
-		const dataService = new PluginDataService(this);
+		await UnicodeSearchPlugin.loadUserSettings();
+
+        const dataService = new PluginDataService(this);
 		const usageTrackedStorage = new UsageTrackedStore(dataService);
 
 		await UnicodeSearchPlugin.initializeData(dataService, new UcdService());
@@ -37,6 +40,8 @@ export default class UnicodeSearchPlugin extends Plugin {
 				return true;
 			},
 		});
+
+        this.addSettingTab(new SettingTab(this.app, this));
 	}
 
 	private static async initializeData(dataService: DataService, ucdService: UcdService): Promise<void> {
@@ -50,6 +55,10 @@ export default class UnicodeSearchPlugin extends Plugin {
 
 		await dataService.exportData(data);
 		await dataService.setAsInitialized();
+	}
+
+	private static async loadUserSettings() {
+		// this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
 	}
 
 }
