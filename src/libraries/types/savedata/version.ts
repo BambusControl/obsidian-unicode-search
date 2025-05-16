de743ff408a40fab3cc514804ab007aee11e8d42
Bambus Control <mojmir.majer@gmail.com>
2025-05-16 23:23:20 +0200

    Version `0.7.1`

--- src/libraries/types/savedata/version.ts
+++ src/libraries/types/savedata/version.ts
@@ -16,12 +16,12 @@ export type PluginVersion
     | "0.6.0"
     | "0.6.1"
     | "0.7.0"
-    | "0.7.0-NEXT"
-    // Update every release only if plugin version changed
+    | "0.7.1"
+    // Update every release
     ;
 
-export type CurrentPluginVersion = "0.7.0-NEXT" & PluginVersion;
-export const CURRENT_PLUGIN_VERSION: CurrentPluginVersion = "0.7.0-NEXT";
+export type CurrentPluginVersion = "0.7.1" & PluginVersion;
+export const CURRENT_PLUGIN_VERSION: CurrentPluginVersion = "0.7.1";
 
 /**
  * Version of the save data schema.
@@ -37,9 +37,8 @@ export type SaveDataVersion = PluginVersion &
     | "0.5.0"
     | "0.6.0"
     | "0.7.0"
-    | "0.7.0-NEXT"
     // Update only if save data schema changed
     );
 
-export type CurrentSaveDataVersion = "0.7.0-NEXT" & SaveDataVersion;
-export const CURRENT_DATA_VERSION: CurrentPluginVersion = "0.7.0-NEXT";
+export type CurrentSaveDataVersion = "0.7.0" & SaveDataVersion;
+export const CURRENT_DATA_VERSION: SaveDataVersion = "0.7.0";
