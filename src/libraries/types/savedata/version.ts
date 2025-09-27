534787af2b88af398d8a0feb9409ba2a901c99d3
Mojmír Majer <mojmir.majer@gmail.com>
2025-09-27 15:49:19 +0200

    Set version as 0.7.3-NEXT

--- src/libraries/types/savedata/version.ts
+++ src/libraries/types/savedata/version.ts
@@ -19,11 +19,12 @@ export type PluginVersion
     | "0.7.1"
     | "0.7.2"
     | "0.7.3"
+    | "0.7.3-NEXT"
     // Update every release
     ;
 
-export type CurrentPluginVersion = "0.7.3" & PluginVersion;
-export const CURRENT_PLUGIN_VERSION: CurrentPluginVersion = "0.7.3";
+export type CurrentPluginVersion = "0.7.3-NEXT" & PluginVersion;
+export const CURRENT_PLUGIN_VERSION: CurrentPluginVersion = "0.7.3-NEXT";
 
 /**
  * Version of the save data schema.
